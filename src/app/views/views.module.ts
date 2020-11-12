7f51a1564a0bd7afc7ae7638a0c0366d5b6685b4
Kassio <kassio.schaider@hotmail.com>
2020-11-12 04:52:14 -0300

    Ajutes

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { HomeComponent } from './home/home.component';
 import { ProdutoComponent } from './home/produto/produto.component';
 import { ProdutoListComponent } from './home/produto-list/produto-list.component';
